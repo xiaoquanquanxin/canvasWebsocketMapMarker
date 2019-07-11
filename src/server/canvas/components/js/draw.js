9deedc620e616090c530a6222ea5f33465433eb6
xiaoquanquanxin <xxkl123@126.com>
2019-07-11 15:57:58 +0800

    绘制了两种状态

--- src/server/canvas/components/js/draw.js
+++ src/server/canvas/components/js/draw.js
@@ -1,3 +1,5 @@
+//  抽象方法
+
 //  绘制路径
 /**
  * @startPoint：起始点
@@ -42,6 +44,91 @@ function drawImage(img, point, width, height) {
     ctx.drawImage(img, point.x, point.y, width, height);
 }
 
+
+//  实例方法
+
+//  绘制地图
+function drawMap() {
+    drawImage(ImageMap, {x: 0, y: 0}, canvas.width, canvas.height);
+}
+
+//  绘制全部站点
+function drawStations() {
+    //  绘制全部站点
+    StationList.forEach(function (item, index) {
+        drawStation(item, ImageStationBasic);
+    });
+}
+
+//  绘制路径
+function drawRoad() {
+    //  绘制路径
+    PointsList.reduce(function (prev, current) {
+        drawLine(calculatePoint(prev), calculatePoint(current), 'rgba(0,199,0,0.4)', 15);
+        return current;
+    });
+
+    //  绘制拐弯
+    PointsList.forEach(function (item, index) {
+        let __point = calculatePoint(item);
+        //  绘制某个点
+        drawRound(__point, 7.5, 'rgba(0,199,0,0.4)',);
+        // console.log(ImageCar, __point, 26, 36);
+        // drawImage(ImageStation, {x: __point.x - 26 / 2, y: __point.y - 36 / 2}, 26, 36);
+    });
+}
+
+
+//  绘制某个站点
+/**
+ * @point：点位
+ * @type：点位类型
+ * */
+function drawStation(point, img) {
+    let __point = calculatePoint(point);
+    __point.x = __point.x - img.width / 2;
+    __point.y = __point.y - img.height;
+    drawImage(img, __point, img.width, img.height);
+}
+
+//  对外暴露方法
+
+//  绘制起点终点
+function drawStartAndEnd(startPoint, endPoint) {
+    //  绘制起点需要全部擦除
+    drawClear();
+    //  绘制地图
+    drawMap();
+    //  绘制道路
+    drawRoad();
+    //  绘制全部站点
+    drawStations();
+    //  绘制起点和终点
+    startPoint && drawStation(startPoint, ImageStationStart);
+    endPoint && drawStation(endPoint, ImageStationEnd);
+}
+
+//  绘制未定位状态
+function drawUnLocation() {
+    //  任何时候都要先晴空
+    drawClear();
+    //  绘制地图
+    drawImage(ImageMap, {x: 0, y: 0}, canvas.width, canvas.height);
+    //  绘制道路
+    drawRoad();
+    //  绘制全部站点
+    drawStations();
+}
+
+//  绘制用户开启定位状态
+function drawLocation() {
+    //  先画未定位
+    drawUnLocation();
+    //  用户定位
+    let __userPoint = calculatePoint(userPoint);
+    drawImage(ImageUser, __userPoint, ImageUser.width, ImageUser.height);
+}
+
 //  绘制用户
 function drawUser(point, radio, fillStyle) {
     ctx.beginPath();
@@ -56,35 +143,29 @@ function drawUser(point, radio, fillStyle) {
 //  主绘制
 //  封装了绘制路线和地图
 function mainRender() {
+    return;
     //  任何时候都要先晴空
     drawClear();
     //  绘制地图
     drawImage(ImageMap, {x: 0, y: 0}, canvas.width, canvas.height);
+
+    //  测试
     (function () {
+        return;
         // 测试四角--证明坐标系准确性
-        __testCorner(bottom_differ, left_differ, bottomLineParams.k * leftLineParams.k)
+        __testCorner(bottom_differ, left_differ, bottomLineParams.k * leftLineParams.k);
+        console.clear();
+        //  测试点位
+        drawRound(calculatePoint(TestPoint), 10, 'red');
     }());
-    console.clear();
 
-    //  绘制路径
-    pointsList.reduce(function (prev, current) {
-        drawLine(calculatePoint(prev), calculatePoint(current), 'blue', 15);
-        return current;
-    });
 
-    //  绘制拐弯
-    pointsList.forEach(function (item, index) {
-        let __point = calculatePoint(item);
-        //  绘制某个点
-        drawRound(__point, 7.5, 'blue');
-        // console.log(ImageCar, __point, 26, 36);
-        drawImage(ImageStation, {x: __point.x - 26 / 2, y: __point.y - 36 / 2}, 26, 36);
-    });
+    //  绘制道路
+    drawRoad();
+    //  绘制全部站点
+    drawStations();
 
-    //  绘制某个实际的点位
-    let __point = calculatePoint(StartPoint);
-    //  绘制某个点
-    drawRound(__point, 4.5, 'yellow');
+    return;
 
     //  绘制小车
     drawImage(ImageCar, {x: 0, y: 0}, ImageCar.width, ImageCar.height);
@@ -100,4 +181,4 @@ function drawClear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-//  
+//  todo    考虑这些小图点载入顺序，考虑小图的大小比例
